5d931afa8684fec88009902a6da98f1da178fb03
MansoorCM <manzoorcm96@gmail.com>
2024-01-09 11:55:12 +0530

    Added photos fragment and viewmodel

---
+++ app/src/main/java/com/example/photos/PhotosFragment.kt
@@ -0,0 +1,28 @@
+package com.example.photos
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.photos.databinding.FragmentPhotosBinding
+
+class PhotosFragment : Fragment() {
+
+    private val photosViewModel: PhotosViewModel by viewModels()
+    private lateinit var binding: FragmentPhotosBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPhotosBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
---
+++ app/src/main/java/com/example/photos/PhotosViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.photos
+
+import androidx.lifecycle.ViewModel
+
+class PhotosViewModel: ViewModel() {
+}
